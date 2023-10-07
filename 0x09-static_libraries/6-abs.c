88973494a8d26a9cfe8f1afccbb53bbe90f34d7b
hamza <ha440z.df@gmail.com>
2023-10-07 19:48:45 +0200

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absoulute value of an integer
+ * @n: the int to be check
+ * Return: the absoulute value of int
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
