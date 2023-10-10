96d6171478aff94bc9a6cd1836a37a75a80975a4
hamza <ha440z.df@gmail.com>
2023-10-10 17:41:01 +0200

    1-init

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,4 @@
 #ifndef DOG_H
-
 #define DOG_H
 /**
  * struck dog - a dog's basic info
@@ -25,5 +24,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-
 #endif
